d4e5d962f1e954b896973f4151c3c8d9f0a0ca5a
Mosesthetencommandments <764856533@qq.com>
2020-05-29 10:25:28 +0800

    1

---
+++ App.vue
@@ -0,0 +1,36 @@
+
+<script>
+	export default {
+		onLaunch: function() {
+			
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		},
+		onLoad() {
+			// this.wxicon()
+		},
+		created(){
+			// this.$store.dispatch("initCar")
+		},
+		data(){
+			return{
+			}
+		},
+		methods:{
+			
+		}
+	}
+</script>
+
+<style>
+	page{
+		height: 100%;
+	}
+	::-webkit-scrollbar{
+		display: none;
+	}
+</style>
